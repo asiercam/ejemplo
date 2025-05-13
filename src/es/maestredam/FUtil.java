4566aac08d51b7148f6f7b77914d2e27f0566d6b
asiershyy <asier19704@gmail.com>
2025-05-13 13:17:00 +0200

    Asier Carreteo - Commit Ejer 12

--- src/es/maestredam/FUtil.java
+++ src/es/maestredam/FUtil.java
@@ -5,7 +5,14 @@ import java.io.File;
  *  Práctica de Git/ Entornos
  */
 public class FUtil {
-
+	
+	/**
+	 * Verifica si un archivo con el nombre especificado existe en el sistema de archivos.
+	 *
+	 * @param filename el nombre del archivo a verificar; no puede ser nulo ni una cadena vacía o con solo espacios
+	 * @return  si el archivo existe devuleve true, en caso contrario false
+	 * @throws IllegalArgumentException si el nombre del archivo es nulo, vacío o contiene solo espacios en blanco
+	 */
 	public static boolean existe(String filename) {
 	    if (filename == null || filename.trim().isEmpty()) {
 	        throw new IllegalArgumentException("El nombre de archivo no puede estar vacío, ni contener solo espacios");
